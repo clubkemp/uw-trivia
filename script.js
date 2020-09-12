f283e1ec9187e841e1ff977c5e9b2c22e9a3d253
Jonny K <Kempj2.jk@gmail.com>
2020-09-11 19:31:13 -0700

    adding game content container to be populated

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 //TODO: setup your DOM Variables
+// var jumbotronDiv  = document.querySelector(".jumbotron")
+var gameContentDiv = document.querySelector("#game-content")
+var jumboContentDiv = document.querySelector("#jumbo-content")
+var startBtn = document.querySelector("#play-button")
+
+
+var clockH3 = document.querySelector("#clock")
+var time = 10
+
 // TODO:Start the game when play is pressed
+startBtn.addEventListener("click", function(event){
+    jumboContentDiv.style.display = "none"
+    gameContentDiv.setAttribute("class", "col")
+
+})
     //TODO: Create clock
     // TODO: replace hero with UL questions
 //TODO: use setInterval to start counting down, every time the clock ticks adjust the clock on the DOM

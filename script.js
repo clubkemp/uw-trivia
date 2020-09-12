0f017d9342c14f9f5926bcc600d97448d5874a35
Jonny K <Kempj2.jk@gmail.com>
2020-09-12 16:22:04 -0700

    Timers working

--- script.js
+++ script.js
@@ -6,14 +6,32 @@ var startBtn = document.querySelector("#play-button")
 
 
 var clockH3 = document.querySelector("#clock")
-var time = 10
-
+var time = 3
 // TODO:Start the game when play is pressed
-startBtn.addEventListener("click", function(event){
-    jumboContentDiv.style.display = "none"
-    gameContentDiv.setAttribute("class", "col-12 mt-5")
+function timer(){
+    var counter = setInterval(startTime,1000);
+    function startTime(){
+        time--
+        console.log(time)
+        if(time === 0){
+            //TODO:Endgame
+            clearInterval(counter);
+        }
+    }
+}
 
+//Start Game!
+startBtn.addEventListener("click", function(event){
+    //turn off the jumbotron intro section
+    jumboContentDiv.style.display = "none"
+    //TODO:Access the game object and grab first trivia card
+    //turn on the game content
+    gameContentDiv.setAttribute("class", "col-12 mt-5")
+    timer();
 })
+
+
+
     //TODO: Create clock
     // TODO: replace hero with UL questions
 //TODO: use setInterval to start counting down, every time the clock ticks adjust the clock on the DOM

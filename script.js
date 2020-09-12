9c9d40299275bd590dd7338b81bfd1b700fb3ea1
Jonny K <Kempj2.jk@gmail.com>
2020-09-11 17:25:44 -0700

    adding a clock element to HTML

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("Hello world");
+//TODO: setup your DOM Variables
+// TODO:Start the game when play is pressed
+    //TODO: Create clock
+    // TODO: replace hero with UL questions
+//TODO: use setInterval to start counting down, every time the clock ticks adjust the clock on the DOM
+// TODO: every time the player gets an answer right add to score
+// TODO: every time the player gets an answer wrong, subtract time.
+

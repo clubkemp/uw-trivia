39540e45b8ebc495736787e2661d6a088f2743a1
Jonny K <Kempj2.jk@gmail.com>
2020-09-12 22:12:01 -0700

    loading images into assets, updating src value in triva obj

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 //Create triva card deck and answer bucket
 var answerBucket = ["3 Muskateers", "100 Grand", "Almond Joy", "Baby Ruth","Butterfinger", "Cadbury Carmel Egg", "Charleston Chew", "Heath", "Mars Bar", "Milky Way", "Payday", "Rolo", "Snickers", "Tootsie Roll" ]
 var cardDeck = [
-  { title: "3 Muskateers", imgSrc: "http://placekitten.com/300/300", index:1},
-  { title: "100 Grand", imgSrc: "src", index:2},
-  { title: "Almond Joy", imgSrc: "src", index:3},
-  { title: "Butterfinger", imgSrc: "src", index:4},
-  { title: "Cadbury Carmel Egg", imgSrc: "src", index:5},
-  { title: "Charleston Chew", imgSrc: "src", index:6},
-  { title: "Heath", imgSrc: "src", index:7},
-  { title: "Mars Bar", imgSrc: "src", index:8},
-  { title: "Milky Way", imgSrc: "src", index:9},
-  { title: "Payday", imgSrc: "src", index:10},
-  { title: "Rolo", imgSrc: "src", index:11},
-  { title: "Snickers", imgSrc: "src", index:12},
-  { title: "Tootsie Roll", imgSrc: "src", index:13}
+  { title: "3 Muskateers", imgSrc: "./assets/3muskateers.JPG", index:1},
+  { title: "100 Grand", imgSrc: "./assets/100grand.jpeg", index:2},
+  { title: "Almond Joy", imgSrc: "./assets/almondJoy.JPG", index:3},
+  { title: "Butterfinger", imgSrc: "./assets/butterfinger.jpeg", index:4},
+  { title: "Cadbury Carmel Egg", imgSrc: "./assets/cadburyCarmelEgg.jpeg", index:5},
+  { title: "Charleston Chew", imgSrc: "./assets/charlestonChew.jpeg", index:6},
+  { title: "Heath", imgSrc: "./assets/heath.jpeg", index:7},
+  { title: "Mars Bar", imgSrc: "./assets/mars.jpeg", index:8},
+  { title: "Milky Way", imgSrc: "./assets/milkyWay.jpeg", index:9},
+  { title: "Payday", imgSrc: "./assets/payday.jpeg", index:10},
+  { title: "Rolo", imgSrc: "./assets/rolo.jpeg", index:11},
+  { title: "Snickers", imgSrc: "./assets/snickers.jpeg", index:12},
+  { title: "Tootsie Roll", imgSrc: "./assets/tootsieROll.jpg", index:13}
 ];
 
 //DOM Variables
@@ -92,12 +92,16 @@ function cardBuild(i) {
     gameContentDiv.setAttribute("class", "col-12 mt-5");
     //TODO:Set the answer content using the answers Array
     gameContentDiv.querySelector("img").setAttribute("src", currentCard[0].imgSrc);
+    
     gameContentDiv.querySelector("#a1").setAttribute("value", answers[0]);
     gameContentDiv.querySelector("#a1-label").textContent=answers[0];
+    
     gameContentDiv.querySelector("#a2").setAttribute("value", answers[1]);
     gameContentDiv.querySelector("#a2-label").textContent=answers[1];
+    
     gameContentDiv.querySelector("#a3").setAttribute("value", answers[2]);
     gameContentDiv.querySelector("#a3-label").textContent=answers[2];
+    
     gameContentDiv.querySelector("#a4").setAttribute("value", answers[3]);
     gameContentDiv.querySelector("#a4-label").textContent=answers[3];
 
